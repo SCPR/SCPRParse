27d99a3bc5a06feaf289e501bfd94be7a4b71fae
Jeremy Hoffing <jhoffing@scpr.org>
2014-09-03 17:23:39 -0700

    Refactor email send-off job

--- cloud/pfs.js
+++ cloud/pfs.js
@@ -1,34 +1,23 @@
 Parse.Cloud.job("sendPFSEmail", function(request, status) {
-    var Mailgun = require('mailgun');
-    Mailgun.initialize('sandboxed0e928a89f043bf9593550b03de91e8.mailgun.org', 'key-c5fb719216d46d9be9d8bf80bfb468ad');
-
     var query = new Parse.Query("PFSUser")
     query.find({
         success: function(results) {
             for (var i = 0; i< results.length; i++) {
                 var object = results[i];
-                email = object.get('email');
-                membershipToken = object.get('sustainingMembershipToken')
-                Mailgun.sendEmail({
-                  to: email,
-                  from: "Mailgun@CloudCode.com",
-                  subject: "Welcome to KPCC's Pledge Free Stream!",
-                  text: "You can now access our pledge-free live stream at: http://scprdev.org/listen_live?parse_key=" + membershipToken + " with a maximum of 3 separate listening sessions"
-                }, {
-                  success: function(response) {
-                    console.log(response);
-                  },
-                  error: function(response) {
-                    console.error(response);
-                  }
-                }).then(function() {
+                if (object.get('emailSent') == false) {
+                    sendMail(object).then(function() {
                         // Set the job's success status
                         status.success("Email send-off job completed successfully.");
+                        object.set("emailSent", true);
+                        object.save();
                         }, function(error) {
                         // Set the job's error status
                         status.error("Uh oh, something went wrong.");
-                        });
-
+                    });
+                }
+                else {
+                    status.success("Email send-off job completed");
+                }
             }
         },
         error: function(error) {
@@ -36,3 +25,23 @@ Parse.Cloud.job("sendPFSEmail", function(request, status) {
         }
     });
 });
+
+function sendMail(userObject) {
+    var Mailgun = require('mailgun');
+    Mailgun.initialize('sandboxed0e928a89f043bf9593550b03de91e8.mailgun.org', 'key-c5fb719216d46d9be9d8bf80bfb468ad');
+
+    var sendMailGun = Mailgun.sendEmail({
+        to: userObject.get('email'),
+        from: "scprdev@scpr.org",
+        subject: "Welcome to KPCC's Pledge Free Stream!",
+        text: "You can now access our pledge-free live stream at: http://scprdev.org/listen_live?parse_key=" + userObject.get('sustainingMembershipToken') + " with a maximum of 3 separate listening sessions"
+        }, {
+         success: function(response) {
+            console.log(response);
+         },
+         error: function(response) {
+            console.error(response);
+         }
+    });
+    return sendMailGun;
+}
