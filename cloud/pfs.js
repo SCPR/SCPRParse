3f45cf4ca3b9bd260903ff8693ec65eead7cf282
Jeremy Hoffing <jhoffing@scpr.org>
2014-09-18 14:56:18 -0700

    Update token name

--- cloud/pfs.js
+++ cloud/pfs.js
@@ -34,7 +34,7 @@ function sendMail(userObject) {
         to: userObject.get('email'),
         from: "scprdev@scpr.org",
         subject: "Welcome to KPCC's Pledge Free Stream!",
-        text: "You can now access our pledge-free live stream at: http://scprdev.org/listen_live?parse_key=" + userObject.get('sustainingMembershipToken') + " with a maximum of 3 separate listening sessions"
+        text: "You can now access our pledge-free live stream at: http://localhost:3000/listen_live/pledge-free?pledgeToken=" + userObject.get('pledgeToken') + " with a maximum of 5 separate listening sessions"
         }, {
          success: function(response) {
             console.log(response);
