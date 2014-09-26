90ee9561fc1587d11118a4aa649e0c9d1334bd9c
Jeremy Hoffing <jhoffing@scpr.org>
2014-09-26 11:00:33 -0700

    Update email body and add PFSBatchEmail function

--- cloud/pfs.js
+++ cloud/pfs.js
@@ -1,4 +1,33 @@
-Parse.Cloud.job("sendPFSEmail", function(request, status) {
+Parse.Cloud.job("sendPFSBatchEmail", function(request, status) {
+    var query = new Parse.Query("PFSUser")
+    query.notEqualTo("emailSent", true);
+    query.equalTo("recordSource", "batch");
+    query.find({
+        success: function(results) {
+            if (results.length > 0) {
+                for (var i = 0; i< results.length; i++) {
+                    var object = results[i];
+                    sendMail(object).then(function() {
+                        // Set the job's success status
+                        status.success("Email send-off job completed successfully.");
+                        object.set("emailSent", true);
+                        object.save();
+                        }, function(error) {
+                        // Set the job's error status
+                        status.error("Uh oh, something went wrong.");
+                    });
+                }
+            } else {
+                status.success("No users to email.");
+              }
+        },
+        error: function(error) {
+            console.log("Error" + error.code + " " + error.message);
+        }
+    });
+});
+
+Parse.Cloud.job("sendPFSTransactionalEmail", function(request, status) {
     var query = new Parse.Query("PFSUser")
     query.notEqualTo("emailSent", true);
     query.find({
@@ -29,12 +58,12 @@ Parse.Cloud.job("sendPFSEmail", function(request, status) {
 function sendMail(userObject) {
     var Mailgun = require('mailgun');
     Mailgun.initialize('sandboxed0e928a89f043bf9593550b03de91e8.mailgun.org', 'key-c5fb719216d46d9be9d8bf80bfb468ad');
-
+    var emailBody = "Dear " + userObject.get('name') + ", We’re excited to introduce our newest member benefit, the Pledge-Free Stream! As a way of thanking you for your incredible generosity, you can now stream KPCC on your computer during our member drives - without any fundraising interruptions. How does it work? Active during our pledge drives, the Pledge-Free Stream carries all of the programming you hear on-air, but removes the fundraising breaks and replaces them with additional news segments and other0 great content you hear on KPCC year-round. Check out the FAQ page to learn more. It’s the same KPCC you know and love, just without the pledge drive! As a current member, you’re receiving access to the pledge-free stream for this drive automatically, so you don’t need to do anything else to start streaming a pledge-free KPCC. The Pledge-Free Stream is live starting tomorrow, October 8th – the same day the Fall Drive begins. Use this link to listen to the stream on your desktop or mobile web browser: www.scprdev.org/pledge-free/pledgeToken=" + userObject.get('pledgeToken') + "Thanks again for your support! <br/>Sincerely, <br/>Rob Risko"
     var sendMailGun = Mailgun.sendEmail({
         to: userObject.get('email'),
         from: "scprdev@scpr.org",
         subject: "Welcome to KPCC's Pledge Free Stream!",
-        text: "You can now access our pledge-free live stream at: http://localhost:3000/listen_live/pledge-free?pledgeToken=" + userObject.get('pledgeToken') + " with a maximum of 5 separate listening sessions"
+        text: emailBody You can now access our pledge-free live stream at: http://localhost:3000/listen_live/pledge-free?pledgeToken=" + userObject.get('pledgeToken') "
         }, {
          success: function(response) {
             console.log(response);
