99d2d9178afc12002cd6022d5ab3536e3fc492e3
Sean Dillingham <sean.dillingham@gmail.com>
2016-06-14 16:33:29 -0700

    update recordSource for fiscal year-end 2016 drive

--- cloud/pfs.js
+++ cloud/pfs.js
@@ -1,11 +1,11 @@
 // Require Underscore.js
 var _ = require('underscore');
- 
+
 Parse.Cloud.job("sendPFSTransactionalEmail", function(request, status) {
   var startingTime = new Date().getTime();
   var query = new Parse.Query("PfsUser")
   query.notEqualTo("emailSent", true);
-  query.equalTo("recordSource", "spring16pledge");
+  query.equalTo("recordSource", "fye16plus");
   query.find({
       success: function(results) {
           if (results.length > 0) {
@@ -25,7 +25,7 @@ Parse.Cloud.job("sendPFSTransactionalEmail", function(request, status) {
                 var elapsedTime = new Date().getTime() - startingTime;
                 status.success("Just sent " + results.length + " emails in " + elapsedTime + "ms");
               });
- 
+
           } else {
               status.success("No users to email.");
             }
@@ -35,7 +35,7 @@ Parse.Cloud.job("sendPFSTransactionalEmail", function(request, status) {
       }
   });
 });
- 
+
 Parse.Cloud.job("sendPFSBatchEmail", function(request, status) {
   var startingTime = new Date().getTime();
   var query = new Parse.Query("PfsUser")
@@ -60,7 +60,7 @@ Parse.Cloud.job("sendPFSBatchEmail", function(request, status) {
                 var elapsedTime = new Date().getTime() - startingTime;
                 status.success("Just sent " + results.length + " emails in " + elapsedTime + "ms");
               });
- 
+
           } else {
               status.success("No users to email.");
             }
@@ -70,17 +70,17 @@ Parse.Cloud.job("sendPFSBatchEmail", function(request, status) {
       }
   });
 });
- 
- 
- 
+
+
+
 function sendMail(userObject, callback) {
     var Mailgun = require('mailgun');
     Mailgun.initialize('email.scprdev.org', 'key-c5fb719216d46d9be9d8bf80bfb468ad');
-     
+
     // Transactional Drive Pledges
     var emailSubject = "Welcome to KPCC Plus!"
     var emailBody = "Dear " + userObject.get('name') + ", <br/><p>Congratulations! You can now stream KPCC on your computer or mobile device during our member drives - without any fundraising interruptions.</p><p>The fundraising-free stream KPCC Plus is easy to access. Click or paste this link to listen on your desktop or mobile web browser: http://www.scpr.org/listen_live/pledge-free?pledgeToken=" + userObject.get('pledgeToken') + "</p><p>You can also access KPCC Plus directly through our iPhone App. Launch the app, and tap on KPCC Live in the orange navbar at the top of the screen. Choose the KPCC Plus stream from the menu, and type this code: " + userObject.get('pledgeToken') + "</p><p>Thanks again for your generous support! Your contribution will go right back into the balanced coverage and inspiring stories you love.</p><p></br><br/>Sincerely, <br/>Rob Risko</p><p>P.S. Having trouble accessing KPCC Plus? Call us at 626-583-5121 or visit our FAQ page: http://www.kpcc.org/plus</p>"
- 
+
     Mailgun.sendEmail({
         to: userObject.get('email'),
         from: "Rob Risko <membership@kpcc.org>",
